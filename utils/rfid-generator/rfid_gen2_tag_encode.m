d5697bd56ce32bfe91e325ec7d888fca18cee93a
Bruno <bruno.espinozaamaya@uqconnect.edu.au>
2015-05-11 18:04:40 +1000

    RFID Encoding accurate

--- utils/rfid-generator/rfid_gen2_tag_encode.m
+++ utils/rfid-generator/rfid_gen2_tag_encode.m
@@ -32,18 +32,25 @@ end
 fm0_one_bits = [1 1];
 fm0_zero_bits = [1 -1];
 
+miller_zero_bits = [1 1];
+miller_one_bits = [1 -1];
+
 preamble_mask = [];
 preamble_tag = [];
 one_fm0 = [];
 zero_fm0 = [];
 
-fm0_samp_rate = samp_rate;
+one_miller = [];
+zero_miller = [];
 
-%generate 0 and 1 for FM0
+%generate 0 and 1 for FM0 and Base Miller
 for i=1:length(fm0_one_bits)
-  one_fm0 = [one_fm0 ones(1,fm0_samp_rate/2)*fm0_one_bits(i)];
-  zero_fm0 = [zero_fm0 ones(1,fm0_samp_rate/2)*fm0_zero_bits(i)];
+  one_fm0 = [one_fm0 ones(1,samp_rate/2)*fm0_one_bits(i)];
+  zero_fm0 = [zero_fm0 ones(1,samp_rate/2)*fm0_zero_bits(i)];
+  one_miller = [one_miller ones(1,samp_rate/2)*miller_one_bits(i)];
+  zero_miller = [zero_miller ones(1,samp_rate/2)*miller_zero_bits(i)];
 end
+
 
 div_val = 2;
 %generate preambles
@@ -62,38 +69,50 @@ elseif (modul_type == 3)
 end
 
 for i=1:length(preamble_mask)
-  preamble_tag = [preamble_tag ones(1,fm0_samp_rate/div_val)*preamble_mask(i)];
+  preamble_tag = [preamble_tag ones(1,samp_rate/div_val)*preamble_mask(i)];
 end
 
-fm0_bits_mask = zeros(2, fm0_samp_rate);
+fm0_bits_mask = zeros(2, samp_rate);
 fm0_bits_mask(1,:) = zero_fm0;
 fm0_bits_mask(2,:) = one_fm0;
 
-%first modulate in FM0, and then multiply by carrier if we are using Miller
-fm0_output = [];
+miller_bits_mask = zeros(2, samp_rate);
+miller_bits_mask(1,:) = zero_miller;
+miller_bits_mask(2,:) = one_miller;
+
+%modulate in FM0 or Miller-Base. Then multiply by carrier if we are using Miller.
+base_output=[];
 l_symbol = 1;
 last_phase = 1;
 
-fm_mill_bits = ~in_bits;
-fm0_miller_out = [];
+if (modul_type > 0)
+    last_phase = -1;
+end
 
 for i=1:length(in_bits)
-  if (l_symbol == 1)
-    last_phase = last_phase * -1;
-  end
-  
-  fm0_output = [fm0_output (fm0_bits_mask(in_bits(i)+1,:) * last_phase)];
-  fm0_miller_out = [fm0_miller_out (fm0_bits_mask(fm_mill_bits(i)+1,:) * last_phase)];
-  l_symbol = in_bits(i);
+
+    if (modul_type == 0) #modulate by FM0
+        if (l_symbol == 1)
+            last_phase = last_phase * -1;
+        end
+        base_output =[base_output (fm0_bits_mask(in_bits(i)+1,:) * last_phase)];
+    else #modulate by Miller
+    
+        if ~(l_symbol == 0 && in_bits(i) == 1)
+            last_phase = last_phase * -1;
+        end
+        base_output =[base_output (miller_bits_mask(in_bits(i)+1,:) * last_phase)];
+    end
+    
+    l_symbol = in_bits(i);
 end
 
 if (modul_type == 0) %if we ask for FM=0, return FM=0.
-  y = [preamble_tag fm0_output]; %preamble + RN16
+  y = [preamble_tag base_output]; %preamble + RN16
   return
 end
 
-%Now handle Miller Encoding - We must generate a carrier and then multiply fm0_output with that.
-
+%now here Miller Encoding
 m2_mask = [1 -1 1 -1];
 m4_mask = [1 -1 1 -1 1 -1 1 -1];
 m8_mask = [1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1];
@@ -119,9 +138,9 @@ end
 
 miller_out = [];
 
-for i=1:samp_rate:length(fm0_output)
-  fm0_d = fm0_miller_out(i:i+samp_rate-1) * -1;
-  miller_out = [miller_out (miller_carrier .* fm0_d)];
+for i=1:samp_rate:length(base_output)
+  miller_d = base_output(i:i+samp_rate-1);
+  miller_out = [miller_out (miller_carrier .* miller_d)];
 end
 
 y = [preamble_tag miller_out];
